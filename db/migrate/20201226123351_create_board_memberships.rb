ebe3a6a748549175434a06df7af44f18dfaa12df
Lomig Enfroy <lomig@hey.com>
2020-12-26 13:41:14 +0100

    Create base models

---
+++ db/migrate/20201226123351_create_board_memberships.rb
@@ -0,0 +1,10 @@
+class CreateBoardMemberships < ActiveRecord::Migration[6.1]
+  def change
+    create_table :board_memberships do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :board, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
