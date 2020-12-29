51fc3ce80b9df37c9254971dad6be6769622a686
Lomig Enfroy <lomig@hey.com>
2020-12-29 05:16:42 +0100

    Add connection button

---
+++ app/components/navbar/connect/component.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+class Navbar::Connect::Component < ApplicationComponent
+end
---
+++ test/components/navbar/connect_component_test.rb
@@ -0,0 +1,10 @@
+require "test_helper"
+
+class Navbar::DisconnectComponentTest < ViewComponent::TestCase
+  def test_component_renders_something_useful
+    # assert_equal(
+    #   %(<span>Hello, components!</span>),
+    #   render_inline(Navbar::DisconnectComponent.new(message: "Hello, components!")).css("span").to_html
+    # )
+  end
+end
