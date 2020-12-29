8246ba981c1304eb34e1c5689161dc2d1d7de96a
Lomig Enfroy <lomig@hey.com>
2020-12-29 04:47:22 +0100

    Fix Devise / Turbo conflicts on redirect

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,6 @@ module TrelloLike
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.eager_load_paths << Rails.root.join('lib/assets')
   end
 end
